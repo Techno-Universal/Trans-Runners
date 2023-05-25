cd74a11bde9fa5f35fd16a70f05ff42c7fad2fb5
Techno-Universal <109560834+Techno-Universal@users.noreply.github.com>
2023-05-25 16:34:38 +1000

    25/05/2023 -1

--- Assets/Scripts/OnlineLobby.cs
+++ Assets/Scripts/OnlineLobby.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Pun.Demo.Cockpit;
+using Photon.Realtime;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -22,21 +23,72 @@ public class OnlineLobby : MonoBehaviourPunCallbacks
 
     void Start()
     {
-        playersReady = new bool[playersReady.Length];
+        playersReady = new bool[PhotonNetwork.CurrentRoom.MaxPlayers];
+        PhotonNetwork.LocalPlayer.NickName = "Player " + PhotonNetwork.LocalPlayer.ActorNumber;
+        roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+
+        numberOfPlayers.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString() + " / " + PhotonNetwork.CurrentRoom.MaxPlayers;
+    }
+
+    public void UpdateName()
+    {
+        PhotonNetwork.LocalPlayer.NickName = playerName.text;
     }
 
     public void LoadLevel()
     {
-        if(PhotonNetwork.IsMasterClient)
+        if(!PhotonNetwork.IsMasterClient)
         {
-            SceneManager.LoadScene("Level1");
+            messages.text = "Illigal action: You are not the host.";
+        }
+        else if (PhotonNetwork.CurrentRoom.PlayerCount < PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            messages.text = "Waiting for players... Lobby still has availability.";
         }
         else
         {
-            
+            SceneManager.LoadScene("Level1");
         }
+        
     }
 
+    [PunRPC]
+    public void ReadyPLayer(int playerNumber, bool isReady)
+    {
+        playersReady[playerNumber] = isReady;
+    }
+
+    public void RunReadyPLayer(bool isReady)
+    {
+        view.RPC("ReadyPlayer", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber, isReady);
+    }
+    bool AllPlayersReady()
+    {
+        foreach(bool item in playersReady)
+        {
+            if (item == false) return false;
+        }
+        return true;
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        numberOfPlayers.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString() + " / " + PhotonNetwork.CurrentRoom.MaxPlayers;
+        Invoke("UpdateBoolsOnJoin", 1);
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerEnteredRoom(otherPlayer);
+        numberOfPlayers.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString() + " / " + PhotonNetwork.CurrentRoom.MaxPlayers;
+        view.RPC("ReadyPlayer", RpcTarget.All, otherPlayer.ActorNumber, false);
+    }
+    void UpdateBoolOnJoin()
+    {
+        int playerNUmber = PhotonNetwork.LocalPlayer.ActorNumber;
+        bool isReady = playersReady[ playerNUmber - 1];
+
+        view.RPC("ReadyPlayer", RpcTarget.All, playerNUmber, isReady);
+    }
     // Update is called once per frame
     void Update()
     {
